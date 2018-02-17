71f3ec0070069d608d0fa48117aa513afe5d3e14
Thales Chagas <thalesgchagas@gmail.com>
2018-02-17 15:43:56 -0200

    Facebook Login
    Adicionado Login com Facebook e armazenando dados de Login no Firebase

--- app/src/main/java/com/our_software_factory/medicinedonate/MainActivity.java
+++ app/src/main/java/com/our_software_factory/medicinedonate/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -18,69 +19,37 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CallbackManager callbackManager;
-    private LoginButton loginButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
         setContentView(R.layout.activity_main);
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList("public_profile"));
+        if (AccessToken.getCurrentAccessToken() == null)
+        {
+            goLoginScreen();
+        }
+    }
 
-        callbackManager = CallbackManager.Factory.create();
+    private void goLoginScreen()
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 
-        // Callback registration
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Intent mintent = new Intent(MainActivity.this, AddAdActivity.class);
-                startActivity(mintent);
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-
-        // Callback registration
-//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                // App code
-//                Intent mintent = new Intent(MainActivity.this, AddAdActivity.class);
-//                startActivity(mintent);
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                // App code
-//            }
-//
-//            @Override
-//            public void onError(FacebookException exception) {
-//                // App code
-//            }
-//        });
+    public void logout(View view)
+    {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 
     public void addButtonClick(View view){
-        Intent mintent = new Intent(MainActivity.this, AddAdActivity.class);
-        startActivity(mintent);
+        Intent intent = new Intent(this, AddAdActivity.class);
+        startActivity(intent);
     }
 
     public void viewButtonClick(View view){
-        Intent mintent = new Intent(MainActivity.this, ViewAdActivity.class);
-        startActivity(mintent);
+        Intent intent = new Intent(this, ViewAdActivity.class);
+        startActivity(intent);
     }
 }
