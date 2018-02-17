41d69e891f31c979ae68d8aa8bd930733f9aabc0
Thales Chagas <thalesgchagas@gmail.com>
2018-02-17 17:11:48 -0200

    Correção Logout

--- app/src/main/java/com/our_software_factory/medicinedonate/MainActivity.java
+++ app/src/main/java/com/our_software_factory/medicinedonate/MainActivity.java
@@ -1,9 +1,11 @@
 package com.our_software_factory.medicinedonate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -14,17 +16,38 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView userNameTextView;
+    private TextView emailTextView;
+    private TextView uidTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (AccessToken.getCurrentAccessToken() == null)
+        userNameTextView = (TextView) findViewById(R.id.textViewUserName);
+        emailTextView = (TextView) findViewById(R.id.textViewEmail);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null)
+        {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+            String uid = user.getProviderId();
+
+            userNameTextView.setText(name);
+            emailTextView.setText(email);
+        }
+        else
         {
             goLoginScreen();
         }
@@ -39,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void logout(View view)
     {
+        FirebaseAuth.getInstance().signOut();
         LoginManager.getInstance().logOut();
         goLoginScreen();
     }
